4f6402379214b2d04346f037ca00c42a66896cca
danjwalton <20973540+danjwalton@users.noreply.github.com>
2022-05-12 00:09:14 +0100

    New datasets

--- datasets/INFORM/informAPI.R
+++ datasets/INFORM/informAPI.R
@@ -5,8 +5,8 @@ inform_get <- function(year, indicator){
   year_wf_id <- data.table(fromJSON(paste0(wf_url, year)))
   wf_id <- year_wf_id[grepl("INFORM Risk|INFORM GRI", Name)][which.max(as.Date(WorkflowDate))]$WorkflowId
   
-  inform_out <- data.table(fromJSON(paste0("https://drmkc.jrc.ec.europa.eu/inform-index/API/InformAPI/countries/Scores/?WorkflowId=", wf_id, "&IndicatorId=",indicator)))
+  inform_out <- data.table(fromJSON(paste0("https://drmkc.jrc.ec.europa.eu/inform-index/API/InformAPI/countries/Scores/?WorkflowId=", wf_id, "&IndicatorId=", indicator)))
   inform_out[, year := year]
   
-  return(inform_out)
+  return(inform_out[])
 }
